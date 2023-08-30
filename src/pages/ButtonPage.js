1de450238454f5aca1c20bfb25481e6f65a36ba0
Sagar Bhagwat <bhagwatsagar2290@gmail.com>
2023-08-30 10:30:33 +0530

    button page rearraged

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,4 +1,10 @@
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import {
+  GoBell,
+  GoCloudDownload,
+  GoDatabase,
+  GoTrashcan,
+  GoX,
+} from "react-icons/go";
 import Button from "../components/Button";
 
 function ButtonPage() {
@@ -6,44 +12,175 @@ function ButtonPage() {
 
   return (
     <div>
-      <div>
-        <Button
-          secondary
-          outline
-          rounded
-          className="mb-5"
-          onClick={handleClick}
-        >
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline onMouseEnter={handleClick}>
-          <GoCloudDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning onMouseLeave={handleClick}>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-      <div>
-        <Button outline rounded className='bg-gray-900'>
-          Something!
-        </Button>
+      <div className="flex">
+        <div className="mx-5">
+          <div>
+            <Button primary rounded className="mb-5" onClick={handleClick}>
+              <GoBell />
+              Click me!!
+            </Button>
+          </div>
+          <div>
+            <Button
+              primary
+              outline
+              rounded
+              className="mb-5"
+              onClick={handleClick}
+            >
+              <GoBell />
+              Click me!!
+            </Button>
+          </div>
+          <div>
+            <Button primary className="mb-5" onClick={handleClick}>
+              <GoBell />
+              Click me!!
+            </Button>
+          </div>
+          <div>
+            <Button primary outline className="mb-5" onClick={handleClick}>
+              <GoBell />
+              Click me!!
+            </Button>
+          </div>
+        </div>
+
+        <div className="mx-5">
+          <div>
+            <Button className="mb-5" success rounded onMouseEnter={handleClick}>
+              <GoCloudDownload />
+              Buy Now!
+            </Button>
+          </div>
+          <div>
+            <Button
+              className="mb-5"
+              success
+              rounded
+              outline
+              onMouseEnter={handleClick}
+            >
+              <GoCloudDownload />
+              Buy Now!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" success onMouseEnter={handleClick}>
+              <GoCloudDownload />
+              Buy Now!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" success outline onMouseEnter={handleClick}>
+              <GoCloudDownload />
+              Buy Now!
+            </Button>
+          </div>
+        </div>
+        <div className="mx-5">
+          <div>
+            <Button className="mb-5" warning rounded onMouseLeave={handleClick}>
+              <GoDatabase />
+              See Deal!
+            </Button>
+          </div>
+          <div>
+            <Button
+              className="mb-5"
+              warning
+              rounded
+              outline
+              onMouseLeave={handleClick}
+            >
+              <GoDatabase />
+              See Deal!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" warning onMouseLeave={handleClick}>
+              <GoDatabase />
+              See Deal!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" warning outline onMouseLeave={handleClick}>
+              <GoDatabase />
+              See Deal!
+            </Button>
+          </div>
+        </div>
+        <div className="mx-5">
+          <div>
+            <Button className="mb-5" secondary rounded>
+              Hide Ads!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" secondary rounded outline>
+              Hide Ads!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" secondary>
+              Hide Ads!
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" secondary outline>
+              Hide Ads!
+            </Button>
+          </div>
+        </div>
+        <div className="mx-5">
+          <div>
+            <Button className="mb-5" danger rounded>
+              <GoTrashcan />
+              Delete
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" danger rounded outline>
+              <GoTrashcan />
+              Delete
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" danger>
+              <GoTrashcan />
+              Delete
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" danger outline>
+              <GoTrashcan />
+              Delete
+            </Button>
+          </div>
+        </div>
+
+        <div className="mx-5">
+          <div>
+            <Button className="mb-5" rounded>
+              <GoX /> Cancel
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" rounded outline>
+              <GoX /> Cancel
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5">
+              <GoX /> Cancel
+            </Button>
+          </div>
+          <div>
+            <Button className="mb-5" outline>
+              <GoX /> Cancel
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
